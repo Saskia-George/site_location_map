408a8124019f48b292d9bf277c97ebfcfe2b5eb9
Saskia <saskia.george@mail.utoronto.ca>
2021-01-09 12:12:22 -0500

    first commit

---
+++ sl.py
@@ -0,0 +1,17 @@
+import random
+import numpy as np
+from typing import List, Dict, Optional, Tuple
+import copy
+
+from site_location import SiteLocationPlayer, Store, SiteLocationMap, euclidian_distances, attractiveness_allocation
+
+class ourClass:
+
+    "really great algorithm below!"
+
+    def place_stores(self, slmap: SiteLocationMap,
+                     store_locations: Dict[int, List[Store]],
+                     current_funds: float):
+
+
+
